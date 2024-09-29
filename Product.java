e2e1773c28865218531d6195c85e5ab1683312b5
Assiyat7 <143499065+Asiyat-d@users.noreply.github.com>
2024-09-29 21:15:08 +0500

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,41 @@
+public class Product implements Cloneable {
+    private String name;
+    private double price;
+
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    protected Product clone() {
+        try {
+            return (Product) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Product [name=" + name + ", price=" + price + "]";
+    }
+}
